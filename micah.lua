2740dcffbec6f4dd7b0c844cc6069fe2ed16b122
Dylan Fenn <dylan.fenn@colorado.edu>
2025-04-02 23:30:59 -0600

    continous

--- micah.lua
+++ micah.lua
@@ -1,14 +1,13 @@
 -- MIDI_TRANSFER
 reaper.gmem_attach("MIDI_TRANSFER")
-reaper.gmem_write(0, 0)
 
 function Main()
-  Data = reaper.gmem_read(0)
+  local Data = reaper.gmem_read(0)
   if Data > 0 then
     reaper.Main_OnCommand(1008, 0) -- Transport: Pause
-    return
+    reaper.gmem_write(0, 0) -- Reset the shared memory slot to prevent repeated triggering
   end
-  reaper.defer(Main)
+  reaper.defer(Main) -- Schedule the Main function to run again
 end
 
-reaper.defer(Main)
+reaper.defer(Main) -- Initial call to start the loop
