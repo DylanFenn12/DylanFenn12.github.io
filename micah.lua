7d2caf59765433c8db9da3c5e6398e7434cd5753
Dylan Fenn <dylan.fenn@colorado.edu>
2025-04-02 23:11:24 -0600

    changed to pause

--- micah.lua
+++ micah.lua
@@ -5,10 +5,14 @@ reaper.gmem_write(0, 0)
 function Main()
   Data = reaper.gmem_read(0)
   if Data > 0 then
-    reaper.Main_OnCommand(1007, 0) -- Transport:Play
+    reaper.Main_OnCommand(1008, 0) -- Transport: Pause
     return
   end
   reaper.defer(Main)
 end
 
 reaper.defer(Main)
+--[[ 
+  This script is designed to be used with the MIDI Transfer extension for Reaper.
+  It listens for a signal from the extension and pauses the transport when it receives data.
+  The script uses gmem to communicate with the MIDI Transfer extension.
