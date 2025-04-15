9a65facff430655c387e3c21a6bb1c3a3fe1e208
Dylan Fenn <dylan.fenn@colorado.edu>
2025-04-15 17:16:28 -0600

    working better

--- wa/wa12/ica12.js
+++ wa/wa12/ica12.js
@@ -5,33 +5,34 @@ const answerButton = document.querySelector('#js-tweet');
 quotebuttonnew.addEventListener('click', getquote);
 answerButton.addEventListener('click', showAuthor);
 
-const endpoint = 'https://zenquotes.io/api/random';
-
 function getquote() {
-console.log('Button clicked!');
-
-fetch(endpoint)
-    .then((response) => {
+    console.log('Button clicked!');
+  
+    const endpoint = 'https://api.allorigins.win/get?url=' + encodeURIComponent('https://zenquotes.io/api/random') + '&disableCache=true';
+  
+    fetch(endpoint)
+      .then((response) => {
         if (!response.ok) {
-        throw new Error('Network response was not ok ' + response.statusText);
+          throw new Error('Network response was not ok ' + response.statusText);
         }
         return response.json();
-    })
-    .then((data) => {
-if (Array.isArray(data) && data.length > 0) {
-    CurrentQuote = data[0];
-    console.log('Random Quote', CurrentQuote.q);
-    displayQuote(CurrentQuote);
-}
-else {
-throw new Error('Unexpected data format');}
-    })
-
-    .catch((error) => {
+      })
+      .then((data) => {
+        const parsed = JSON.parse(data.contents);
+        if (Array.isArray(parsed) && parsed.length > 0) {
+          CurrentQuote = parsed[0];
+          console.log('Random Quote:', CurrentQuote.q);
+          displayQuote(CurrentQuote);
+        } else {
+          throw new Error('Unexpected data format from ZenQuotes');
+        }
+      })
+      .catch((error) => {
         console.error('There was a problem with the fetch operation:', error);
         alert('Error fetching quote. Please try again later.');
-    });
-    }
+      });
+  }
+  
 
 function displayQuote(data) {
 const quoteText = document.getElementById('js-quote-text');
